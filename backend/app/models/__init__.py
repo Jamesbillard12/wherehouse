0546d4d5942235cb62291228e18eb77e3eb66f20
James Billard <Jamesbillard12@hotmail.com>
2026-08-26 20:35:35 -0700

    Export core models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,29 @@
+from app.models.core import (
+    Area,
+    Container,
+    ContainerPlacement,
+    ContainerRelationship,
+    ContainerType,
+    Household,
+    HouseholdRelationship,
+    HouseholdUser,
+    Item,
+    ItemPlacement,
+    User,
+    Zone,
+)
+
+__all__ = [
+    "Area",
+    "Container",
+    "ContainerPlacement",
+    "ContainerRelationship",
+    "ContainerType",
+    "Household",
+    "HouseholdRelationship",
+    "HouseholdUser",
+    "Item",
+    "ItemPlacement",
+    "User",
+    "Zone",
+]
